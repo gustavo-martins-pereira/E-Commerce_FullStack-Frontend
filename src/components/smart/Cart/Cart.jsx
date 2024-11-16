b3f6f00dbebf9830a4d05ba894352b81ca8e521a
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2024-11-16 15:05:08 -0300

    ADD - Remove ProductItem from Cart and "+" and "-" Buttons

--- src/components/smart/Cart/Cart.jsx
+++ src/components/smart/Cart/Cart.jsx
@@ -1,8 +1,10 @@
 import { useContext, useState } from "react";
 import { IoIosClose, IoMdCart } from "react-icons/io";
+import { MdDelete } from "react-icons/md";
 
 import { CartContext } from "@contexts/cartContext";
 import { BadgeNumber } from "@components/dumbs/BadgeNumber/BadgeNumber";
+import { Button } from "@components/dumbs/Button/Button";
 import { bufferArrayToImageURL } from "@utils/bufferArrayToImageURL";
 
 export function Cart() {
@@ -10,33 +12,65 @@ export function Cart() {
     const [isCartOpen, setIsCartOpen] = useState(false);
 
     // CONTEXTS
-    const { cartItems } = useContext(CartContext);
+    const { cartItems, updateCartQuantity, removeItemFromCart } = useContext(CartContext);
 
     function totalPrice() {
-        return Array.from(cartItems.values()).reduce((total, cartItem) => total + (cartItem.quantity * cartItem.price), 0).toFixed(2);
+        return Array.from(cartItems.values())
+            .reduce((total, cartItem) => total + cartItem.quantity * cartItem.price, 0)
+            .toFixed(2);
     }
 
     return (
         <section className={`bg-white w-80 h-full fixed top-0 right-0 flex flex-col gap-4 shadow-lg p-6 transform transition-transform duration-300 ease-in-out z-10 ${isCartOpen ? "translate-x-0" : "translate-x-full"}`}>
             <div className="flex justify-between items-center">
                 <h2 className="text-2xl font-bold">Your Cart</h2>
-                <IoIosClose className="cursor-pointer rounded hover:bg-red-100" size="3rem" color="red" onClick={() => setIsCartOpen(false)} />
+                <IoIosClose
+                    className="cursor-pointer rounded hover:bg-red-100"
+                    size="3rem"
+                    color="red"
+                    onClick={() => setIsCartOpen(false)}
+                />
             </div>
 
             <section className="flex flex-col gap-4 overflow-y-auto scrollbar--left-margin">
-                {Array.from(cartItems.values()).map(cartItem => {
-                    return <article className="flex items-center justify-between" key={cartItem.id}>
+                {Array.from(cartItems.values()).map(cartItem => (
+                    <article className="flex items-center justify-between" key={cartItem.id}>
                         <div className="flex items-center gap-4">
-                            <img className="w-12 h-12 object-fill" src={bufferArrayToImageURL(cartItem.image.data)} alt="" />
+                            <img
+                                className="w-12 h-12 object-fill"
+                                src={bufferArrayToImageURL(cartItem.image.data)}
+                                alt=""
+                            />
+
                             <div>
                                 <h3 className="text-lg font-semibold">{cartItem.name}</h3>
-                                <p className="text-sm text-gray-600">Price: ${cartItem.price}</p>
-                                <p className="text-sm text-gray-600">Quantity: {cartItem.quantity}</p>
+                                <p className="text-sm">Price: ${cartItem.price}</p>
+
+                                <div className="flex items-center gap-2 text-sm">
+                                    Quantity:
+                                    <button
+                                        className="bg-red-100 w-6 h-6 flex justify-center items-center pb-[0.1rem] rounded text-red-800 hover:bg-red-200"
+                                        onClick={() => updateCartQuantity(cartItem.id, cartItem.quantity - 1)}
+                                    >
+                                        -
+                                    </button>
+                                    <p className="text-sm text-gray-600"> {cartItem.quantity}</p>
+                                    <button
+                                        className="bg-green-100 rounded w-6 h-6 flex justify-center items-center md:pb-[0.1rem] lg:pb-[0.2rem] text-green-800 hover:bg-green-200"
+                                        onClick={() => updateCartQuantity(cartItem.id, cartItem.quantity + 1)}
+                                    >
+                                        +
+                                    </button>
+                                </div>
+
+                                <Button className="bg-red-500 hover:bg-red-600 mt-2 rounded p-2 cursor-pointer" onClick={() => removeItemFromCart(cartItem.id)}>
+                                    <MdDelete className="text-red-100" size="1rem"  />
+                                </Button>
                             </div>
                         </div>
                         <p className="text-lg font-bold">${(cartItem.quantity * cartItem.price).toFixed(2)}</p>
                     </article>
-                })}
+                ))}
             </section>
 
             <footer className="flex justify-between border-t pt-4">
@@ -44,22 +78,20 @@ export function Cart() {
                 <p className="text-2xl font-bold">${totalPrice()}</p>
             </footer>
 
-            {/* LABEL OPEN/CLOSE */}
-            {!isCartOpen &&
+            {!isCartOpen && (
                 <div
                     className="bg-white absolute left-0 bottom-4 border-l border-y rounded-l shadow p-2 -translate-x-full cursor-pointer"
                     onClick={() => setIsCartOpen(true)}
                 >
-                    {cartItems.size ?
-                        <BadgeNumber
-                            number={cartItems.size}
-                            accessibilityText="Cart's products quantity"
-                        >
+                    {cartItems.size ? (
+                        <BadgeNumber number={cartItems.size} accessibilityText="Cart's products quantity">
                             <IoMdCart className="text-icon-primary" size="2.5rem" />
-                        </BadgeNumber> :
+                        </BadgeNumber>
+                    ) : (
                         <IoMdCart className="text-icon-primary" size="2.5rem" />
-                    }
-                </div>}
+                    )}
+                </div>
+            )}
         </section>
     );
 }
--- src/contexts/cartContext.jsx
+++ src/contexts/cartContext.jsx
@@ -11,14 +11,42 @@ export function CartProvider({ children }) {
 
             const existingProduct = oldCartItems.get(product.id);
 
-            return existingProduct ? newCartItems.set(product.id, { ...existingProduct, quantity: existingProduct.quantity + quantity }) :
-                newCartItems.set(product.id, { ...product, quantity });
+            return existingProduct
+                ? newCartItems.set(product.id, { ...existingProduct, quantity: existingProduct.quantity + quantity})
+                : newCartItems.set(product.id, { ...product, quantity });
         });
-    };
+    }
+
+    function updateCartQuantity(productId, newQuantity) {
+        setCartItems(oldCartItems => {
+            const newCartItems = new Map(oldCartItems);
+
+            if(newQuantity <= 0) {
+                newCartItems.delete(productId);
+            } else {
+                const existingProduct = oldCartItems.get(productId);
+                if(existingProduct) {
+                    newCartItems.set(productId, { ...existingProduct, quantity: newQuantity });
+                }
+            }
+
+            return newCartItems;
+        });
+    }
+
+    function removeItemFromCart(productId) {
+        setCartItems(oldCartItems => {
+            const newCartItems = new Map(oldCartItems);
+            newCartItems.delete(productId);
+
+            return newCartItems;
+        });
+    }
 
     return (
-        <CartContext.Provider value={{ cartItems, addToCart }}>
+        <CartContext.Provider value={{ cartItems, addToCart, updateCartQuantity, removeItemFromCart }}>
             {children}
         </CartContext.Provider>
     );
 }
+
